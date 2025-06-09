6db6c5db10dcbb7ec1c96396f5fd3d18361b6fc1
Florin Tobler <florin.tobler@hotmail.com>
2025-06-09 14:39:18 +0200

    new project on flortos base

---
+++ yeetos/application/flortos_conf.h
@@ -0,0 +1,28 @@
+/*
+ * flortos_conf.h
+ *
+ *  Created on: Jun 8, 2025
+ *      Author: ftobler
+ */
+
+#ifndef FLORTOS_CONF_H_
+#define FLORTOS_CONF_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define MAY_NUMBER_OF_TASKS 8
+
+// Define the architecture of the scheduler
+#define SCHEDULER_ARCHITECURE_M0plus
+// #define SCHEDULER_ARCHITECURE_M4F
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* FLORTOS_CONF_H_ */
---
+++ yeetos/application/taskmanager.h
@@ -0,0 +1,25 @@
+/*
+ * app.h
+ *
+ *  Created on: Jun 8, 2025
+ *      Author: ftobler
+ */
+
+#ifndef TASKMANAGER_H_
+#define TASKMANAGER_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void taskmanager_start();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* TASKMANAGER_H_ */
